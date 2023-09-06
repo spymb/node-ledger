eac11917744726ac7d5cc45cc6b0d6b6b8c42a9a
spym <spym200@qq.com>
2023-09-06 23:33:32 +0800

    feat: 账目的展示、添加与删除

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,48 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+//导入 lowdb
+const low = require("lowdb");
+const FileSync = require("lowdb/adapters/FileSync");
+const adapter = new FileSync(__dirname + "/../data/db.json");
+//获取 db 对象
+const db = low(adapter);
+
+//导入 shortid
+const shortid = require("shortid");
+
+//记账本的列表
+router.get("/account", function (req, res, next) {
+  //获取所有的账单信息
+  const accounts = db.get("accounts").value();
+  res.render("list", { accounts: accounts });
+});
+
+//添加记录
+router.get("/account/create", function (req, res, next) {
+  res.render("create");
+});
+
+//新增记录
+router.post("/account", (req, res) => {
+  //生成 id
+  const id = shortid.generate();
+  //写入文件
+  db.get("accounts")
+    .unshift({ id: id, ...req.body })
+    .write();
+  //成功提醒
+  res.render("success", { msg: "添加成功哦~~~", url: "/account" });
+});
+
+//删除记录
+router.get("/account/:id", (req, res) => {
+  //获取 params 的 id 参数
+  const id = req.params.id;
+  //删除
+  db.get("accounts").remove({ id: id }).write();
+  //提醒
+  res.render("success", { msg: "删除成功~~~", url: "/account" });
 });
 
 module.exports = router;
